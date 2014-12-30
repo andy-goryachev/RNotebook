0dc4d44227d3e44a82fa3f47acacd472a4b815e8
Andy <andy@goryachev.com>
2014-12-30 15:36:12 -0800

    .

--- src/research/notebook/HelpMenu.java
+++ src/research/notebook/HelpMenu.java
@@ -1,4 +1,4 @@
-// Copyright (c)2014-2015 Andy Goryachev <andy@goryachev.com>
+// Copyright (c) 2014-2015 Andy Goryachev <andy@goryachev.com>
 package research.notebook;
 import goryachev.common.ui.Application;
 import goryachev.common.ui.CAction;
