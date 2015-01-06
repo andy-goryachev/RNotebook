e02b2c0e3962975ca673331dc35ff4ce022644e0
Andy <andy@goryachev.com>
2015-01-05 16:09:45 -0800

    fs

--- src/goryachev/notebook/js/fs/FS.java
+++ src/goryachev/notebook/js/fs/FS.java
@@ -1,11 +1,11 @@
 // Copyright (c) 2015 Andy Goryachev <andy@goryachev.com>
 package goryachev.notebook.js.fs;
 import goryachev.common.util.Dump;
+import goryachev.notebook.js.JsUtil;
 import goryachev.notebook.util.InlineHelp;
 import java.io.File;
 
 
-/** "FS" object in the global context */
 public class FS
 {
 	private File cur;
@@ -39,12 +39,41 @@ public class FS
 	}
 	
 	
+	public long getFreeSpace()
+	{
+		return getFreeSpace(cur());
+	}
+	
+	
+	public long getFreeSpace(Object x)
+	{
+		File f = JsUtil.parseFile(x);
+		return f.getFreeSpace();
+	}
+	
+	
+	public long getTotalSpace()
+	{
+		return getTotalSpace(cur());
+	}
+	
+	
+	public long getTotalSpace(Object x)
+	{
+		File f = JsUtil.parseFile(x);
+		return f.getTotalSpace();
+	}
+	
+	
 	public String toString()
 	{
 		InlineHelp h = new InlineHelp("FS");
-		h.a("provides filesystem operations:");
-		h.a("ls([path],[mode])", "lists files in current directory");
-		h.a("pwd()", "prints current directory");
+		h.a("provides access to the filesystem:");
+		
+		h.a("freeSpace, getFreeSpace(file)", "returns the amount of free space");
+		h.a("ls([path],[mode])", "returns the list of files in the current directory");
+		h.a("pwd()", "returns the current directory");
+		h.a("totalSpace, getTotalSpace(file)", "returns the amount of total space");
 		return h.toString();
 	}
 }
--- src/goryachev/notebook/js/io/OS.java
+++ src/goryachev/notebook/js/io/OS.java
@@ -1,6 +1,7 @@
 // Copyright (c) 2015 Andy Goryachev <andy@goryachev.com>
 package goryachev.notebook.js.io;
 import goryachev.notebook.util.InlineHelp;
+import java.net.InetAddress;
 
 
 public class OS
@@ -47,16 +48,45 @@ public class OS
 	}
 	
 	
+	public String getHostName() throws Exception
+	{
+		return InetAddress.getLocalHost().getHostName();
+	}
+	
+	
+	public String getName()
+	{
+		return System.getProperty("os.name");
+	}
+	
+	
+	public String getVersion()
+	{
+		return System.getProperty("os.version");
+	}
+	
+	
+	public String getArch()
+	{
+		return System.getProperty("os.arch");
+	}
+	
+	
 	public String toString()
 	{
 		InlineHelp h = new InlineHelp("OS");
-		h.a("OS provides access to the following system functions:");
-		h.a("cpuCount", "current time in milliseconds");
-		h.a("time", "current time in milliseconds");
+		h.a("provides access to the following system functions:");
+		
+		h.a("arch", "returns the OS architecture");
+		h.a("cpuCount", "returns number of processor cores");
 		h.a("freeMem", "returns the amount of free memory in the JVM");
+		h.a("hostName", "returns the host name");
 		h.a("maxMem", "returns the maximum amount of memory that the JVM will attempt to use.");
-		h.a("usedMem", "returns the amount of memory currently being used");
+		h.a("name", "returns the OS name");
+		h.a("time", "current time in milliseconds");
 		h.a("totalMem", "returns the total amount of memory in the JVM");
+		h.a("usedMem", "returns the amount of memory currently being used");
+		h.a("version", "returns the OS version");
 		return h.toString();
 	}
 }
