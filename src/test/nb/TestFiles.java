99a916e3f0562af14941743576a6c60056398dcb
Andy <andy@goryachev.com>
2015-02-09 15:58:10 -0800

    .

---
+++ src/test/nb/TestFiles.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 Andy Goryachev <andy@goryachev.com>
+package test.nb;
+import goryachev.common.test.TF;
+import goryachev.common.test.Test;
+import goryachev.common.util.D;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+
+public class TestFiles
+{
+	public static void main(String args[])
+	{
+		TF.run();
+	}
+
+
+	@Test
+	public void test() throws Exception
+	{
+		File f = new File("test/hidden.file");
+		Path p =
+			f.toPath();
+			//Paths.get("test");
+		
+		String attr =
+//			"dos:hidden,*";
+			"dos:*,*";
+//			"*";
+			//"posix:*";
+		
+		Map<String,Object> rv = Files.readAttributes(p, attr, LinkOption.NOFOLLOW_LINKS);
+		D.list(rv);
+	}
+}
