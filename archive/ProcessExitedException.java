f60ffda7bea1915b5f2e3a3c1d027752347dc93e
andy goryachev <andy@goryachev.com>
2023-01-28 20:03:12 -0800

    cleanup

--- archive/ProcessExitedException.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2009-2015 Andy Goryachev <andy@goryachev.com>
-package goryachev.notebook.js;
-
-
-public class ProcessExitedException
-	extends RuntimeException
-{
-}
--- archive/raspberry PI/pi.sh
+++
@@ -1,1 +0,0 @@
-java -cp RNotebook.jar:/opt/pi4j/lib/pi4j-core.jar:/opt/pi4j/lib/pi4j-device.jar:/opt/pi4j/lib/pi4j-gpio-extension.jar:/opt/pi4j/lib/pi4j-service.jar goryachev.notebook.RNotebookApp
